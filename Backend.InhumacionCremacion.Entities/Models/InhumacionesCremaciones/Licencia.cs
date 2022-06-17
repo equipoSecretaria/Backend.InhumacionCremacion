122b817135425b0d3dbcb534b6f625990b9798be
ppalacios <ppalacios@soaint.com>
2022-06-17 16:23:00 -0500

    "Persistencia Base 64"

---
+++ Backend.InhumacionCremacion.Entities/Models/InhumacionesCremaciones/Licencia.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Backend.InhumacionCremacion.Entities.Models.InhumacionCremacion
+
+{
+    public partial class Licencia
+    {
+            public Guid ID_Tabla { get; set; }
+
+            public Guid ID_Documento { get; set; }
+
+            public string NombreDocumento { get; set; }
+
+            public int NumeroTramite { get; set; }
+
+            public int NumeroLicencia { get; set; }
+
+            public DateTime FechaGeneracion { get; set; }
+
+            public string DocumentoBase64 { get; set; }
+
+    }
+}
